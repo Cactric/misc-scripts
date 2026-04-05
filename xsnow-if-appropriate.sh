2db4f53c845832f12672c4ff68a656d4ceceb09c
Cactric <59334627+Cactric@users.noreply.github.com>
2026-04-05 15:50:47 +0100

    xsnow-if-appropriate: exit 0 if xsnow isn't installed

--- xsnow-if-appropriate.sh
+++ xsnow-if-appropriate.sh
@@ -4,7 +4,7 @@ month="$(date '+%m')"
 day="$(date '+%d')"
 
 # Ensure xsnow is installed
-which xsnow >/dev/null 2>&1 || exit
+which xsnow >/dev/null 2>&1 || exit 0
 
 if [ "$month" = '12' ] || { [ "$month" = '01' ] && [ "$day" -lt 7 ];} || [ "$1" = "-f" ]; then
     env -u HOME xsnow -defaults -noconfig -nomenu -notrees -snowflakes 75 -showsanta -santa 1 -nometeors -moon 0 -aurora 0 -offsets -30 -showbirds 0 -stars 0
