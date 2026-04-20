ade5fed28d1b2173f257f6fa35394286e59a0abf
Cactric <59334627+Cactric@users.noreply.github.com>
2026-04-20 13:52:46 +0100

    dates.zsh: print date of oldest backup

--- dates.zsh
+++ dates.zsh
@@ -3,7 +3,10 @@
 lastUpd="$(date '+%a %e %b %Y %H:%M:%S' -r /var/log/pacman.log)"
 echo "Last update: $lastUpd"
 STATE="${XDG_STATE_HOME:-$HOME/.local/state}"
-lastBtl="$(< "$STATE"/last-btl-finished)"
+
+backupHost="$(ls --sort=time --reverse "$STATE"/last-btl-*-finished | head -n 1)"
+lastBtl="$(< ${backupHost})"
 echo "Last backup: $lastBtl"
+
 now="$(date '+%a %e %b %Y %H:%M:%S')"
 echo "Now:         $now"
